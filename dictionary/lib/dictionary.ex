aeda0271a520057f5370e8047003bac308394d08
Álvaro Domínguez López <alvarodl20@outlook.com>
2023-03-02 18:50:48 +0100

    feat(dictionary): improvement of dictionary application

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,11 @@
 defmodule Dictionary do
-  @word_list "assets/words.txt"
-             |> File.read!()
-             |> String.split(~r/(\r\n|\r|\n)/, trim: true)
+  alias Dictionary.Impl.WordList, as: WordList
 
-  def random_word do
-    @word_list
-    |> Enum.random()
-  end
+  @type t :: WordList.t()
+
+  @spec start() :: t
+  defdelegate start, to: WordList
+
+  @spec random_word(t) :: String.t()
+  defdelegate random_word(word_list), to: WordList
 end
---
+++ dictionary/lib/impl/word_list.ex
@@ -0,0 +1,17 @@
+defmodule Dictionary.Impl.WordList do 
+  @type t :: list(String.t())
+
+  @spec start() :: t
+  def start do
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/(\r\n|\r|\n)/, trim: true)
+  end
+  
+  @spec random_word(t) :: String.t()
+  def random_word(word_list) do
+    word_list
+    |> Enum.random()
+  end
+end
